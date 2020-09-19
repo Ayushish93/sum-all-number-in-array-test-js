578a76db2223955773479a1678cb01d877be67b9
Ayushish93 <69399771+Ayushish93@users.noreply.github.com>
2020-09-18 20:23:07 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 function sumItems(array) {
   // Sum all the numbers in the array
+  if (!array.length) return 0;
+    if (Array.isArray(array[0])) return sumItems(array[0]) + sumItems(array.slice(1));
+    return array[0] + sumItems(array.slice(1));
 }
 
 module.exports = sumItems;
